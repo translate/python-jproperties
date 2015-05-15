49aa26f7a95f912338362bb6ba573116f41e47f0
Jerome Leclanche <jerome@leclan.ch>
2015-05-15 23:49:36 +0200

    Add setup files

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ python-jproperties
 ==================
 
 A python library for parsing and handling Java .properties files.
+
+
+Installation
+------------
+
+To build, run the following command:
+
+```
+ $ python setup.py build
+```
+
+To install, preferably into a virtual environment:
+
+```
+ $ python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import distutils.core
+import os.path
+
+import jproperties
+
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
+
+CLASSIFIERS = [
+	"Development Status :: 3 - Alpha",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 2.7",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.3",
+	"Programming Language :: Python :: 3.4",
+	"Topic :: Software Development :: Internationalization",
+	"Topic :: Software Development :: Libraries",
+	"Topic :: Software Development :: Libraries :: Java Libraries",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+distutils.core.setup(
+	name = "jproperties",
+	py_modules = ["jproperties"],
+	author = jproperties.__author__,
+	author_email = jproperties.__email__,
+	classifiers = CLASSIFIERS,
+	description = "Python library for Java .properties file parsing",
+	download_url = "https://github.com/translate/python-jproperties/tarball/master",
+	long_description = README,
+	url = "https://github.com/translate/python-jproperties",
+	version = jproperties.__version__,
+)
